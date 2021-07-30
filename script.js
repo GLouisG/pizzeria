1ebf1a2dae4481d6ad0d75988e08ed63d7ad0bfd
GLouisG <louis.gacho@student.moringaschool.com>
2021-07-30 21:15:17 +0300

    add size constructor

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ var stuffed = new crust(500, "stuffed");
 var nogluten = new crust(200, "gluten free");
 var crispy = new crust(100, "crispy");
 console.log(nogluten);
+
+function size(price, name){
+  this.price = price;
+  this.name = name;
+}

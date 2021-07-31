cfb305ded618be9d65108fd731b4507b6a84eff2
GLouisG <louis.gacho@student.moringaschool.com>
2021-07-31 12:43:49 +0300

    add if statements to assign user input to objects

--- script.js
+++ script.js
@@ -1,20 +1,52 @@
-
+function pizzaGen(){
 function crust(price, name){
   this.price = price;
   this.name = name;
 }
 
-var stuffed = new crust(500, "stuffed");
-var nogluten = new crust(200, "gluten free");
-var crispy = new crust(100, "crispy");
-console.log(nogluten);
+var stuffed = new crust(700, "stuffed");
+var gluten = new crust(500, "gluten free");
+var crisp = new crust(200, "crispy");
+
 
 function size(price, name){
   this.price = price;
   this.name = name;
 }
 var small = new size(500, "small");
-var medium = new size(200, "medium");
+var mid = new size(200, "medium");
 var large = new size(100, "large");
 
-console.log(small);
+$(document).ready(function(){
+
+$("form#new-pizza").submit(function(event){
+  event.preventDefault();
+
+   var inputTopping = document.getElementById("toppings").value;
+    var inputCrust = document.getElementById("crusts").value;
+    var inputSize = document.getElementById("sizes").value;
+    console.log(inputCrust);
+
+    if(inputSize==="large"){
+      var theSize = large;
+    }else if(inputSize==="medium"){
+      var theSize = mid;
+    }else{
+      var theSize = small;
+    }
+    
+    if(inputCrust ==="gluten"){
+      var theCrust = gluten;
+    }else if(inputCrust ==="stuffed"){
+      var theCrust = stuffed;
+    }else{
+      var theCrust = crisp;
+    }
+
+    var total = parseInt(theCrust.price) + parseInt(theSize.price);
+    console.log(theCrust.price);
+    console.log(theCrust.name);
+});
+
+});
+}

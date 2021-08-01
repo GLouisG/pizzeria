8c7ca2855355e822072c139d5108ade44c791796
GLouisG <louis.gacho@student.moringaschool.com>
2021-08-01 21:11:24 +0300

    improve indenting

--- script.js
+++ script.js
@@ -1,93 +1,88 @@
-
-function crust(price, name){
+//business logic
+function crust(price, name) {
   this.price = price;
   this.name = name;
 }
 
 var stuffed = new crust(700, "stuffed");
-var gluten = new crust(500, "gluten free");
+var gluten = new crust(300, "gluten free");
 var crisp = new crust(200, "crispy");
 
 
-function size(price, name){
+function size(price, name) {
   this.price = price;
   this.name = name;
 }
-var small = new size(500, "small");
-var mid = new size(200, "medium");
-var large = new size(100, "large");
+var small = new size(700, "small");
+var mid = new size(800, "medium");
+var large = new size(900, "large");
 
 
-function pizzaGen(){
-$(document).ready(function(){
-  let total;
-$("form#new-pizza").submit(function(event){
-  
-  event.preventDefault();
-   var inputTopping = document.getElementById("toppings").value;
+function pizzaGen() {
+  $(document).ready(function() {
+
+
+    var inputTopping = document.getElementById("toppings").value;
     var inputCrust = document.getElementById("crusts").value;
     var inputSize = document.getElementById("sizes").value;
-    
 
-    if(inputSize==="large"){
+
+    if (inputSize === "large") {
       var theSize = large;
-    }else if(inputSize==="medium"){
+    } else if (inputSize === "medium") {
       var theSize = mid;
-    }else{
+    } else {
       var theSize = small;
     }
-    
-    if(inputCrust ==="GlutenFree"){
+
+    if (inputCrust === "GlutenFree") {
       var theCrust = gluten;
-    }else if(inputCrust ==="stuffed"){
+    } else if (inputCrust === "stuffed") {
       var theCrust = stuffed;
-    }else{
+    } else {
       var theCrust = crisp;
     }
-    
-    total = parseInt(theCrust.price) + parseInt(theSize.price);
-    
+
+    let total = parseInt(theCrust.price) + parseInt(theSize.price)
+
+
+
+    //user interface logic
+    $("form#new-pizza").submit(function(event) {
+      event.preventDefault();
 
     var mushroom = document.getElementById("mushCheck").checked;
     var salami = document.getElementById("salCheck").checked;
     var pepperoni = document.getElementById("pepCheck").checked;
 
-    if(mushroom===true){
-      $("ul#extras").append("<li>Mushroom</li>");
-    }
-    if(salami===true){
-      $("ul#extras").append("<li>Salami</li>");
-    }
-    if(pepperoni===true){
-      $("ul#extras").append("<li>Pepperoni</li>");
-    }
+      if (mushroom === true) {
+        $("ul#extras").append("<li>Mushroom</li>");
+      }
+      if (salami === true) {
+        $("ul#extras").append("<li>Salami</li>");
+      }
+      if (pepperoni === true) {
+        $("ul#extras").append("<li>Pepperoni</li>");
+      }
 
-    $("#receipt").append( '<div class="row entry">'+
-    '<div class="col-md-3"> <p>'  + inputTopping  + '</p></div>' +
-    '<div class="col-md-3"><p>' + inputSize+ '</p></div>'  +
-    '<div class="col-md-3"><p>' + inputCrust + '</p></div>'  +
-    '<div class="col-md-3"><p>' + total + '</p></div>' +
-  '</div>');
+      $("#receipt").append('<div class="row entry">' +
+        '<div class="col-md-3"> <p>' + inputTopping + '</p></div>' +
+        '<div class="col-md-3"><p>' + inputSize + '</p></div>' +
+        '<div class="col-md-3"><p>' + inputCrust + '</p></div>' +
+        '<div class="col-md-3"><p>' + total + '</p></div>' +
+        '</div>');
 
-  $(".deliver").click(function(){
-    prompt("Where should we deliver to?");
-    alert("your order will be delivered to your location");
-    $(".del-fee").toggle();
+      $(".deliver").click(function() {
+        prompt("Where should we deliver to?");
+        alert("your order will be delivered to your location");
+        $(".del-fee").toggle();
+      });
+
+      $(".pick").click(function() {
+        alert("Hope to see you soon.");
+      });
+    });
+
   });
-  
-  $(".pick").click(function(){
-    alert("Hope to see you soon.");
-  });
-
-return total
-})
-
-console.log(total);
-
-});
 
 }
-
-
-
-
